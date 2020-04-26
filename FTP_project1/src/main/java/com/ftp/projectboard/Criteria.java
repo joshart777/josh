e5c1381a094ae3d03d9e849a54b865f4b9fbd79f
yongdae <yongdae@josh.skbroadband>
2020-04-26 13:44:31 +0900

    0426_게시판_페이징 처리 수정

---
+++ FTP_project1/src/main/java/com/ftp/projectboard/Criteria.java
@@ -0,0 +1,95 @@
+package com.ftp.projectboard;
+
+public class Criteria {
+	
+    private int page;
+    private int perPageNum;
+    
+	private String board_seq;
+	private String board_title;
+	private String board_writer_name;
+	private String board_content_text;
+	private String board_reg_data;
+	private String board_notice;
+	private String board_num;
+
+		   
+    public int getPageStart() {
+        return (this.page-1)*perPageNum;
+    }
+    
+    public Criteria() {
+        this.page = 1;
+        this.perPageNum = 10;
+    }
+    
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        if(page <= 0) {
+            this.page = 1;
+        } else {
+            this.page = page;
+        }
+    }
+    public int getPerPageNum() {
+        return perPageNum;
+    }
+    public void setPerPageNum(int pageCount) {
+        int cnt = this.perPageNum;
+        if(pageCount != cnt) {
+            this.perPageNum = cnt;
+        } else {
+            this.perPageNum = pageCount;
+        }
+    }
+    
+    
+    
+    public String getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(String board_num) {
+		this.board_num = board_num;
+	}
+	public String getBoard_reg_data() {
+		return board_reg_data;
+	}
+	public void setBoard_reg_data(String board_reg_data) {
+		this.board_reg_data = board_reg_data;
+	}
+	public String getBoard_notice() {
+		return board_notice;
+	}
+	public void setBoard_notice(String board_notice) {
+		this.board_notice = board_notice;
+	}
+
+	
+	public String getBoard_seq() {
+		return board_seq;
+	}
+	public void setBoard_seq(String board_seq) {
+		this.board_seq = board_seq;
+	}
+	public String getBoard_title() {
+		return board_title;
+	}
+	public void setBoard_title(String board_title) {
+		this.board_title = board_title;
+	}
+	public String getBoard_writer_name() {
+		return board_writer_name;
+	}
+	public void setBoard_writer_name(String board_writer_name) {
+		this.board_writer_name = board_writer_name;
+	}
+	public String getBoard_content_text() {
+		return board_content_text;
+	}
+	public void setBoard_content_text(String board_content_text) {
+		this.board_content_text = board_content_text;
+	}
+
+}
